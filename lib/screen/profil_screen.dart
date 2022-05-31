4e3171d9d75c6b9a6e2b58de86660d508b508f51
yusuf <yusufanwar1923@gmail.com>
2022-05-31 15:39:37 +0700

    penembahan profil

---
+++ lib/screen/profil_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Profil_Screen extends StatefulWidget {
+  const Profil_Screen({Key? key}) : super(key: key);
+
+  @override
+  State<Profil_Screen> createState() => _Profil_ScreenState();
+}
+
+class _Profil_ScreenState extends State<Profil_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
